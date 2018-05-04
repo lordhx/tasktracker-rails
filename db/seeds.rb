baffdce03dcc5e4279e6ab7b856c4f6dc8800a48
lord_HX <nikita.j.kharitonov@gmail.com>
2018-05-05 01:42:37 +0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Users..."
+default_password = '1q2w3e4r'
+
+alice = User.create(role: :regular, email: 'alice@example.com', password: default_password)
+bob = User.create(role: :regular, email: 'bob@example.com', password: default_password)
+
+junior_manager = User.create(role: :manager, email: 'junior.manager@example.com', password: default_password)
+senior_manager = User.create(role: :manager, email: 'senior.manager@example.com', password: default_password)
+
+puts "Creating Issues..."
+Issue.create(description: 'Is it safe to give you my credit card credentials?', author: bob)
+
+Issue.create(description: 'Bob doesn''t talk to me', author: alice)
+Issue.create(description: 'Where is my cookies?', author: alice, status: :in_progress, assignee: junior_manager)
+Issue.create(description: 'Can''t login into the system', author: alice, status: :resolved, assignee: senior_manager)
